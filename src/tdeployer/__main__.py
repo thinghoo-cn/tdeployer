986f2dc13800eca3d6b57e99390dd88013bda7a3
svtter <svtter@qq.com>
2022-05-12 15:22:32 +0800

    feat: add command

--- src/tdeployer/__main__.py
+++ src/tdeployer/__main__.py
@@ -1,6 +1,14 @@
+import argparse
+
 from tdeployer.application import Application
+from tdeployer.config import logger
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='tdeployer is a automation deployer')
+    parser.add_argument('command', choices=['update', 'update_code', 'deploy'], help='execute command')
+
+    args = parser.parse_args()
+    logger.info(f'command is: {args.command}')
     conf = Application.config_loader()
     Application(config=conf, )
